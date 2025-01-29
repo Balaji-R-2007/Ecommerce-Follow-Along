01495ea9ebb1e613a80da26f75674c3b71a9c1e1
Balaji <RKB@archlinux.localdomain>
2025-01-29 16:27:46 +0530

    Milestone-5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from './Routes.js';
+import { SignupPage } from './Routes.js';
 import "./App.css";
+// import SignupPage from './pages/SignupPage.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
     // <h1 className='bg-black font-bold text-8xl text-white'> tailwindcss</h1>
