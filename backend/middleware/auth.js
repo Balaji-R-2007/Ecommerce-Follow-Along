cb4df53fb141bbfdd49419979d26ae2a22a0e5d4
balaji-r-2007 <balaji648balaji@gmail.com>
2025-04-30 10:15:00 +0530

    milestone-34

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,27 @@
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+  
+  if (!token) {
+    return next(new ErrorHandler("Please login to access this resource", 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret");
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("Invalid or expired token", 401));
+  }
+});
+
+exports.isAdmin = catchAsyncErrors(async (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return next(new ErrorHandler("Only admin can access this resource", 403));
+  }
+  next();
+});
